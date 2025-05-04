c6dc62206f4875741a402663a898305eab45cf20
孤风雪影 <efairy520@qq.com>
2025-05-04 23:02:06 +0800

    定义数据包结构体

--- app.c
+++ app.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include "xnet_tiny.h"
 
 int main (void) {
     setvbuf(stdout, NULL, _IONBF, 0);  // 禁用缓冲，保证 printf 立刻显示
 
+    xnet_init();
+
     printf("xnet running\n");
 
     while (1) {
+        xnet_poll();
     }
 
     return 0;
--- xnet_tiny.h
+++ xnet_tiny.h
@@ -5,4 +5,23 @@
 #ifndef XNET_TINY_H
 #define XNET_TINY_H
 
+#include <stdint.h>
+
+#define XNET_CFG_PACKET_MAX_SIZE        1516        // 收发数据包的最大大小
+
+/**
+ * 网络数据结构
+ */
+typedef struct _xnet_packet_t{
+    uint16_t size;                              // 包中有效数据大小
+    uint8_t * data;                             // 包的数据起始地址
+    uint8_t payload[XNET_CFG_PACKET_MAX_SIZE];  // 最大负载数据量
+}xnet_packet_t;
+
+xnet_packet_t * xnet_alloc_for_send(uint16_t data_size);
+xnet_packet_t * xnet_alloc_for_read(uint16_t data_size);
+
+void xnet_init (void);
+void xnet_poll(void);
+
 #endif // XNET_TINY_H
